7c0e869406a6c8cda1c7373eaa6b30afee125f1f
Victor Hugo Martínez González <le19080710@itmerida.edu.mx>
2021-03-15 00:14:39 -0500

    Update Ghosts.java
    null pointer correction and, improves runaway and pressure

--- src/main/java/aaaimx/gen2021/reactiveGhosts/Ghosts.java
+++ src/main/java/aaaimx/gen2021/reactiveGhosts/Ghosts.java
@@ -6,13 +6,14 @@ import java.util.Map;
 import java.util.Random;
 
 import pacman.controllers.GhostController;
+import pacman.game.Constants.DM;
 import pacman.game.Constants.GHOST;
 import pacman.game.Constants.MOVE;
 import pacman.game.Game;
 
 public final class Ghosts extends GhostController{
-	int close = 2;
-	int pillClose = 2;
+	int close = 3;
+	int pillClose = 10;
 	Random rnd = new Random();
 	EnumMap<GHOST,MOVE> moves=new EnumMap<GHOST,MOVE>(GHOST.class);
 	
@@ -22,11 +23,12 @@ public final class Ghosts extends GhostController{
 				MOVE[] possibilitiesMoves= game.getPossibleMoves(game.getGhostCurrentNodeIndex(ghost),game.getGhostLastMoveMade(ghost));
 				if (game.getGhostEdibleTime(ghost)>0||closeToPower(game)) {
 					// meth for runway
-					moves.put(ghost, getPersureMove(game, ghost, possibilitiesMoves));
+					moves.put(ghost, getRunAwayeMove(game, ghost, possibilitiesMoves));
+					//moves.put(ghost, getPersureMove(game, ghost, possibilitiesMoves));
 				}else {
 					// meth for persure
-					//moves.put(ghost, getPersureMove(game, ghost, possibilitiesMoves));
-					moves.put(ghost, getRunAwayeMove(game, ghost, possibilitiesMoves));
+					moves.put(ghost, getPersureMove(game, ghost, possibilitiesMoves));
+					//moves.put(ghost, getRunAwayeMove(game, ghost, possibilitiesMoves));
 				}
 			}
 		}
@@ -44,30 +46,6 @@ public final class Ghosts extends GhostController{
 		return false;
 		
 	}
-	private MOVE getPersureMove(Game game, GHOST ghost, MOVE[] possibilitiesMoves) {
-		Map <MOVE, Integer> allMovesValues = new HashMap <MOVE,Integer>(possibilitiesMoves.length);
-		int ghLocation = game.getGhostCurrentNodeIndex(ghost);
-		int pcLocation = game.getPacmanCurrentNodeIndex();
-		for (MOVE move : possibilitiesMoves) {
-			int neighbour=game.getNeighbour(ghLocation,move);
-			int distanceValue = game.getShortestPathDistance(pcLocation, neighbour);
-			if (distanceValue > 0) {
-				allMovesValues.put(move, distanceValue);
-			}
-		}
-		// best move
-		
-		int bestDistance = 0;
-		MOVE bestMove = null;
-		for (MOVE move : possibilitiesMoves) {
-			if (allMovesValues.get(move)>bestDistance) {
-				bestDistance=allMovesValues.get(move);
-				bestMove=move;
-			}
-		}
-		
-		return bestMove;
-	}
 	private MOVE getRunAwayeMove(Game game, GHOST ghost, MOVE[] possibilitiesMoves) {
 		Map <MOVE, Integer> allMovesValues = new HashMap <MOVE,Integer>(possibilitiesMoves.length);
 		int ghLocation = game.getGhostCurrentNodeIndex(ghost);
@@ -75,22 +53,79 @@ public final class Ghosts extends GhostController{
 		for (MOVE move : possibilitiesMoves) {
 			int neighbour=game.getNeighbour(ghLocation,move);
 			int distanceValue = game.getShortestPathDistance(pcLocation, neighbour);
-			if (distanceValue > 0) {
-				allMovesValues.put(move, distanceValue);
+			for (GHOST otherGhost: GHOST.values()) {				
+				int oGhLocation = game.getGhostCurrentNodeIndex(otherGhost);
+				int distancePcGh = game.getShortestPathDistance(pcLocation, oGhLocation);
+				if (ghost != otherGhost && distancePcGh>0) {					
+					int[] path= game.getShortestPath(pcLocation, oGhLocation); 
+					path= game.getShortestPath(pcLocation, oGhLocation);
+					for (int node : path) {
+						if (node == neighbour && game.isGhostEdible(otherGhost)) {
+							distanceValue +=50;
+						}else if (node == neighbour){
+							distanceValue +=90;
+						}
+					}
+					allMovesValues.put(move, distanceValue);
+				}
 			}
 		}
 		// best move
 		
-		int bestDistance = Integer.MAX_VALUE;
-		MOVE bestMove = null;
-		for (MOVE move : possibilitiesMoves) {
-			if (allMovesValues.get(move)<bestDistance) {
-				bestDistance=allMovesValues.get(move);
-				bestMove=move;
+			int bestDistance = Integer.MIN_VALUE;
+			MOVE bestMove = null;
+			for (MOVE move : possibilitiesMoves) {
+				if(allMovesValues.get(move)!= null)
+					if (allMovesValues.get(move)>bestDistance) {
+						bestDistance=allMovesValues.get(move);
+						bestMove=move;
+					}
 			}
-		}
+			
+			return bestMove;
 		
-		return bestMove;
+		
+	}
+	private MOVE getPersureMove(Game game, GHOST ghost, MOVE[] possibilitiesMoves) {
+		Map <MOVE, Integer> allMovesValues = new HashMap <MOVE,Integer>(possibilitiesMoves.length);
+		int ghLocation = game.getGhostCurrentNodeIndex(ghost);
+		int pcLocation = game.getPacmanCurrentNodeIndex();
+		for (MOVE move : possibilitiesMoves) {
+			int neighbour=game.getNeighbour(ghLocation,move);
+			int distanceValue = game.getShortestPathDistance(pcLocation, neighbour);
+			for (GHOST otherGhost: GHOST.values()) {	
+				int oGhLocation = game.getGhostCurrentNodeIndex(otherGhost);
+				int distancePcGh = game.getShortestPathDistance(pcLocation, oGhLocation);
+				if (ghost != otherGhost && distancePcGh>0) {					
+					int[] path= game.getShortestPath(pcLocation, oGhLocation); 
+					path= game.getShortestPath(pcLocation, oGhLocation);
+					for (int node : path) {
+						if (node == neighbour && game.isGhostEdible(otherGhost)) {
+							distanceValue -=50;
+						}else if (node == neighbour){
+							distanceValue -=90;
+						}
+					}
+					allMovesValues.put(move, distanceValue);
+				}
+			}
+			
+		}
+		// best move
+		
+			int bestDistance = Integer.MAX_VALUE;
+			MOVE bestMove = null;
+			
+			for (MOVE move : possibilitiesMoves) {
+				if(allMovesValues.get(move)!= null)
+					if (allMovesValues.get(move)<bestDistance) {
+						bestDistance=allMovesValues.get(move);
+						bestMove=move;
+					}
+			} 
+			return bestMove;
+		
+
 	}
 	
 }
